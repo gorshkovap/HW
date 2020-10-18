0c1bec1ba6fde401d537f3296300d96cbeb18521
vpsetter <timurfakhrutdinov@gmail.com>
2020-10-18 22:56:25 +0300

    Добавил комплект решений 7-го задания

---
+++ 7-й семинар/7.1 Header.hpp
@@ -0,0 +1,165 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+
+bool is_equal(double a, double b);
+
+struct Point
+{
+	Point();
+	Point(const double a, const double b);
+
+	friend std::ostream& operator<<(std::ostream& stream, const Point& point);
+
+	double x;
+	double y;
+};
+
+class Vector
+{
+public:
+	Vector();
+	Vector(double a, double b);
+
+	double len() const;
+
+	double x;
+	double y;
+};
+
+double vec_mul(const Vector& v_1, const Vector& v_2);
+
+double scal_mul(const Vector& v_1, const Vector& v_2);
+
+Vector operator-(const Vector& v_1, const Vector& v_2);
+
+bool is_coll(const Vector& v_1, const Vector& v_2);
+
+bool is_perp(const Vector& v_1, const Vector& v_2);
+
+class Shape
+{
+protected:
+	Shape();
+	Shape(const Point& point);
+
+public:
+	virtual ~Shape() = default;
+
+	virtual void print() const = 0;
+	virtual double P() const = 0;
+	virtual double S() const = 0;
+
+	friend std::ostream& operator<<(std::ostream& stream, const Shape& shape);
+
+protected:
+	Point m_point;
+};
+
+class Circle : public Shape
+{
+public:
+	Circle();
+	Circle(const Point& point, const double r);
+	virtual ~Circle() = default;
+	
+	virtual void print() const override;
+	virtual double P() const override;
+	virtual double S() const override;
+
+protected:
+	static const double pi;
+	double m_a; //радиус
+};
+
+class Ellipse : public Circle
+{
+public:
+	Ellipse();
+	Ellipse(const Point& point, const double a, const double b);
+	virtual ~Ellipse() = default;
+
+	virtual void print() const override;
+	virtual double P() const override;
+	virtual double S() const override;
+
+protected:
+	double m_b; //малая полуось
+};
+
+class Polygon : public Shape
+{
+protected:
+	Polygon();
+	Polygon(const Point& point, const Vector& v_1, const Vector& v_2);
+	virtual ~Polygon() = default;
+
+	virtual void print() const override = 0;
+	virtual double P() const = 0;
+	virtual double S() const = 0;
+
+protected:
+	Vector side_1;
+	Vector side_2;
+};
+
+class Triangle : public Polygon
+{
+public:
+	Triangle();
+	Triangle(const Point& point, const Vector& v_1, const Vector& v_2);
+	virtual ~Triangle() = default;
+
+	virtual void print() const override;
+	virtual double P() const override;
+	virtual double S() const override;
+};
+
+class Square : public Polygon
+{
+public:
+	Square();
+	Square(const Point& point, const Vector& v_1, const Vector& v_2);
+	virtual ~Square() = default;
+
+	virtual void print() const override;
+	virtual double P() const override;
+	virtual double S() const override;
+};
+
+class Rectangle : public Polygon
+{
+public:
+	Rectangle();
+	Rectangle(const Point& point, const Vector& v_1, const Vector& v_2);
+	virtual ~Rectangle() = default;
+
+	virtual void print() const override;
+	virtual double P() const override;
+	virtual double S() const override;
+};
+
+class Parallelogram : public Polygon
+{
+public:
+	Parallelogram();
+	Parallelogram(const Point& point, const Vector& v_1, const Vector& v_2);
+	virtual ~Parallelogram() = default;
+
+	virtual void print() const override;
+	virtual double P() const override;
+	virtual double S() const override;
+};
+
+class Rhombus : public Polygon
+{
+public:
+	Rhombus();
+	Rhombus(const Point& point, const Vector& v_1, const Vector& v_2);
+	virtual ~Rhombus() = default;
+
+	virtual void print() const override;
+	virtual double P() const override;
+	virtual double S() const override;
+};
---
+++ 7-й семинар/7.1 main.cpp
@@ -0,0 +1,28 @@
+#include "7.1 Header.hpp"
+
+int main()
+{
+	Point point = { 0,0 };
+
+	std::vector <Shape*> shapes;
+
+	shapes.push_back(new Ellipse(point, 1.0, 2.0));
+	shapes.push_back(new Circle(point, 5.0));
+
+	Vector v_1 = { 5, 0 }, v_2 = { 0, 5 }, v_3 = { 4, 3 }, v_4 = { 0, 6 };
+
+
+	shapes.push_back(new Square(point, v_1, v_2));
+	shapes.push_back(new Triangle(point, v_1, v_2));
+	shapes.push_back(new Rectangle(point, v_1, v_2));
+	shapes.push_back(new Rhombus(point, v_1, v_2));
+	shapes.push_back(new Parallelogram(point, v_1, v_2));
+	/*shapes.push_back(new Square(point, v_1, v_3));*/ // - действительно не работает
+	/*shapes.push_back(new Square(point, v_1, v_4));*/ // - действительно не работает
+
+	for (auto e : shapes)
+	{
+		std::cout << *e << "\n\n";
+	}
+	return 0;
+}
